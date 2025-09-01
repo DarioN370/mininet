d310356f9c72bf1bbedb6dbe02dac85274b2e42f
DarioN370 <netobis2015@gmail.com>
2025-09-01 01:27:29 -0300

    Emcapsulamento adicionado

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         User usuario1 = new User(); // pedindo pra construir ele do zero eu uso o NEW
-        usuario1.id = 1;
-        usuario1.name = "Dario Miranda";
-        usuario1.username = "IamD4rio";
-        usuario1.email = "dario.neto@sou.unifeob.edu.br";
+        usuario1.setId(1);
+        usuario1.setName("Dario Miranda");
+        usuario1.setUsername("IamD4rio");
+        usuario1.setEmail("dario.neto@sou.unifeob.edu.br");
 
         usuario1.displayProfile(); //abre e fecha parenteses para executar ação
 
@@ -15,5 +15,6 @@ public class App {
         p1.author = usuario1;
 
         p1.displayPost();
+        System.out.println("ID DO USUARIO: " + usuario1.getId()); //aqui eu uso o getId(), ele vai la na class User.java, pega o valor do ID, e me mostra aqui
     }
 }
--- src/User.java
+++ src/User.java
@@ -5,13 +5,52 @@
 // o nome da classe tem que ser o mesmo do arquivo .java
 public class User {
     //ATRIBUTOS = O que a classe tem
-    int id;
-    String name; //nome da pessoa
-    String username; //nome de usuario
-    String email;
+    private int id;
+    private String name; //nome da pessoa
+    private String username; //nome de usuario
+    private String email;
 
     //MÉTODOS = O que a classe faz = são executores
     void displayProfile(){
-        System.out.println("O Dono desse perfil é "+ name); //comando pra escrever na tela "sistema, escreva na tela tal coisa, o que esta em parenteses" o ln pula uma lina quando ele escrever
+        System.out.println(" Dono desse perfil: "+ name); //comando pra escrever na tela "sistema, escreva na tela tal coisa, o que esta em parenteses" o ln pula uma lina quando ele escrever
     } //o tipo VOID significa que eu vou executar o metodo mas não vou devolver nenhum dado
+
+    //metodos acessores e modificadores = GETTER e SETTER (get e set)
+    public int getId(){
+        return id;
+    } //usando o metodo para pegar o id
+
+    public void setId(int id){
+        this.id = id;
+    } //void pq ele só pega o valor e guarda, o argumento (), mostra o que você vai pegar, e o tipo que ce ta pegando - Quando voce usa o This, vc pega o da classe, ou seja, aqui eu mexo no ID da classe, e guardando nele o ID que vou receber de outro lugar 
+
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
